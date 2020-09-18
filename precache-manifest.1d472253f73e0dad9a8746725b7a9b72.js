207541f913a543c936600cf9941028fe58a2fbaf
rahul <rahul.kumardeveloper84@gmail.com>
2020-09-18 20:35:52 +0530

    Updates

---
+++ precache-manifest.1d472253f73e0dad9a8746725b7a9b72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50d98280868b27b9e56c",
+    "url": "/DashboardTemplate/static/js/runtime~main.50d98280.js"
+  },
+  {
+    "revision": "fca77277b70e45c61bb5",
+    "url": "/DashboardTemplate/static/js/main.fca77277.chunk.js"
+  },
+  {
+    "revision": "44de571c34b0c4c9033f",
+    "url": "/DashboardTemplate/static/js/1.44de571c.chunk.js"
+  },
+  {
+    "revision": "fca77277b70e45c61bb5",
+    "url": "/DashboardTemplate/static/css/main.6c9edf96.chunk.css"
+  },
+  {
+    "revision": "bb31bfb2654f706bfa6212d987cf8c71",
+    "url": "/DashboardTemplate/index.html"
+  }
+];
